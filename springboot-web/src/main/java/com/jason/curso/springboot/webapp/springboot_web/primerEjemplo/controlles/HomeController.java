57e675d8b17c2aa4b6d7dea5c0cdf722a7c26e93
JotaTorresC <jasons.torresc@uqvirtual.edu.co>
2026-07-06 02:26:02 -0500

    redirigir vistas y ejecutar app en la termianl

---
+++ springboot-web/src/main/java/com/jason/curso/springboot/webapp/springboot_web/primerEjemplo/controlles/HomeController.java
@@ -0,0 +1,17 @@
+package com.jason.curso.springboot.webapp.springboot_web.primerEjemplo.controlles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    //en este caso estamos ingresando a la vista list cuando ingresamos localHost:8080 y se logra porque tenemos entre las rutas
+    // las comillas vacias "" o con el / le podemos dar la ruta que queramos esta es como la base
+    @GetMapping({"", "/", "home"})//con llaves para darle varias rutas
+    public String home() {
+
+        return "redirect:/list";
+        //return "forward:/list";
+    }
+}
